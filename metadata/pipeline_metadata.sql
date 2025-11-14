6f0515cf37f1221295a45678cbeaf92277fa3949
Raj Kamal <145188917+kamalraaz@users.noreply.github.com>
2025-11-14 18:25:50 +0530

    Update pipeline_metadata.sql

--- metadata/pipeline_metadata.sql
+++ metadata/pipeline_metadata.sql
@@ -1,1 +1,10 @@
 
+-- Create metadata table to track last processed date
+CREATE TABLE IF NOT EXISTS gds_de_bootcamp.default.pipeline_metadata (
+    table_name STRING,
+    last_processed_date DATE
+);
+
+-- Insert initial record if no entry exists
+INSERT INTO gds_de_bootcamp.default.pipeline_metadata
+VALUES ("booking_customer_pipeline", DATE("1900-01-01"));
